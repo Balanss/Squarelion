5886292b268e2073a0ef759c988ea0723d2130d7
unknown <gilmonpurry@hotmail.com>
2023-06-23 13:58:16 -0300

    firstpush

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://github.com/Balanss/Squarelion.git',
+  base:'https://Balanss.github.io/Squarelion',
   plugins: [react()],
 })
