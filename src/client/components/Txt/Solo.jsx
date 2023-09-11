e6188ce264b16503a7c2d1c8da5cfe4aadc8a782
gilmon <gilmonpurry@hotmail.com>
2023-09-11 13:09:58 -0300

    fixed pdf-docs

--- src/client/components/Txt/Solo.jsx
+++ src/client/components/Txt/Solo.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 
- function Solo({ createPdf,isChecked ,date,uniqueId,type,month,page,imageUrl ,boosting,post,subject,round}) {
+ function Solo({ createPdf,isChecked ,date,uniqueId,type,month,page,imageUrl ,boosting,post,subject}) {
 
 
   const sendToZapier = async (payload) => {
-    const zapierURL = import.meta.env.VITE_ZAP_SEND_PDF;
+    const zapierURL = import.meta.env.VITE_ZAP_SOLO_PDF;
     try {
       const response = await fetch(zapierURL, {
         method: 'POST',
@@ -22,22 +22,8 @@ import React from 'react';
 
 
 
-  const handleDownload = async() => {
+  const handleDownload = () => {
 
-
-    // async function handleTest(){
-    //   try {
-    //     await Promise.all(
-    //       round.map(async (data) => {
-    //         await addDoc(collection(db, 'Testing'), data);
-    //       })
-    //     );
-    //     console.log('Data sent to Firebase successfully.');
-    //   } catch (error) {
-    //     console.error('Error sending data to Firebase:', error);
-    //   }
-    // }
-    
 
     const currentDate = new Date();
 
@@ -53,44 +39,14 @@ import React from 'react';
     
       postID:uniqueId,
        channel: type,
-      //  textContent: createPdf,
-      //   month:month,
+       textContent: createPdf,
+        month:month,
        name:page,
        postDate:postDate,
-      //  images:imageUrl,
-      //  boosting:boosting,
-      //  subject:subject,
-        post:round[0],
-        post2:round[1],
-        post3:round[2],
-        post4:round[3],
-        post5:round[4],
-        post6:round[5],
-        post7:round[6],
-        post8:round[7],
-        post9:round[8],
-        post10:round[9],
-        post11:round[10],
-        post12:round[11],
-        post13:round[12],
-        post14:round[13],
-        post15:round[14],
-        post16:round[15],
-        post17:round[16],
-        post18:round[17],
-        post19:round[18],
-        post20:round[19],
-        post21:round[20],
-        post22:round[21],
-        post23:round[22],
-        post24:round[23],
-        post25:round[24],
-        post26:round[25],
-        post27:round[26],
-        post28:round[27],
-        post29:round[28],
-        post30:round[29],
-
+       images:imageUrl,
+       boosting:boosting,
+       subject:subject,
+        post:post,
      };
  
      try {
