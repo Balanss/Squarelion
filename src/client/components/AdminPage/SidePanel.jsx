eac83a0a63b4325152888cf1225d4358fe050aaf
gilmon <gilmonpurry@hotmail.com>
2023-10-23 12:19:25 -0300

    v 99

--- src/client/components/AdminPage/SidePanel.jsx
+++ src/client/components/AdminPage/SidePanel.jsx
@@ -1,109 +1,138 @@
-import React,{useEffect,useState} from 'react'
-import usersPic from '../images/new-arrival.png';
-import IN from '../images/in.png';
-import homeBtn from '../images/home-button.png';
-import client from '../images/client.png';
-import survey from '../images/survey.png';
-import sqlProps from '../images/sqlProp.jpg';
-import AdminSurvey from './AdminSurvey';
-import rightArrow from '../images/arrow-right.png';
-import userPfp from "../images/user.png"
-import { useNavigate,Link} from 'react-router-dom';
-import { auth, fs,db } from '/src/client/Firebase.jsx'
+import React, { useEffect, useState } from "react";
+import usersPic from "../images/new-arrival.png";
+import IN from "../images/in.png";
+import homeBtn from "../images/home-button.png";
+import client from "../images/client.png";
+import survey from "../images/survey.png";
+import sqlProps from "../images/sqlProp.jpg";
+import AdminSurvey from "./AdminSurvey";
+import rightArrow from "../images/arrow-right.png";
+import userPfp from "../images/user.png";
+import { useNavigate, Link } from "react-router-dom";
+import { auth, fs, db } from "/src/client/Firebase.jsx";
 
-export default function SidePanel({level,user,switching,setSwitching}) {
-    const navigate = useNavigate()
+export default function SidePanel({ level, user, switching, setSwitching }) {
+  const navigate = useNavigate();
 
-    const handleLogout = () => {
-        auth.signOut().then(() => {
-          navigate('/')
-          window.location.reload()
-        })
-      };
-      
-      function handleGoPfP(){
-              localStorage.setItem('user',user)
-              setTimeout(() =>{
-                  navigate(`/user/${user}`)
-              },1000)
-      }
+  const handleLogout = () => {
+    auth.signOut().then(() => {
+      navigate("/");
+      window.location.reload();
+    });
+  };
 
+  function handleGoPfP() {
+    localStorage.setItem("user", user);
+    setTimeout(() => {
+      navigate(`/user/${user}`);
+    }, 1000);
+  }
 
-
-      const [isHovered, setIsHovered] = useState(false);  
-      const [hiding,setHiding] = useState(true);
-
-
-
-
-      
+  const [isHovered, setIsHovered] = useState(false);
+  const [hiding, setHiding] = useState(true);
 
   return (
-   <aside id="cta-button-sidebar" 
-      className="fixed top-0 left-0 z-40 w-1/5 h-screen transition-transform -translate-x-full sm:translate-x-0"
-      aria-label="Sidebar">
+    <aside
+      id="cta-button-sidebar"
+      className="fixed top-0 left-0 z-40 w-1/5 xl:w-[10%] h-screen transition-transform -translate-x-full sm:translate-x-0"
+      aria-label="Sidebar"
+    >
       <div className="h-full px-3 py-4 overflow-y-auto bg-gray-800 dark:bg-gray-800">
-         <ul className="space-y-2 font-medium">
+        <ul className="space-y-2 font-medium">
+          <li>
+            <span className="flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group">
+              <span className="ml-3">Dashboard</span>
+            </span>
+          </li>
+          <li>
+            <Link
+              to="/"
+              className="transform transition-transform ease-in hover:scale-105 cursor-pointer flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group"
+            >
+              <img
+                src={homeBtn}
+                className="w-6 md:w-[30px] lg:w-[40px] flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"
+              />
+              <span className="md:text-sm lg:text-md ml-3 text-white rounded-lg dark:text-white">
+                Home
+              </span>
+            </Link>
+          </li>
+          <li>
+            <span
+              className="transform transition-transform ease-in hover:scale-105 cursor-pointer flex items-center p-2 rounded-lg dark:text-white dark:hover:bg-gray-700 group"
+              onClick={() => setSwitching("Client")}
+            >
+              <img className="w-6 md:w-[30px] lg:-[40px]" src={client} />
+              <span className="md:text-sm lg:text-md flex-1 ml-3 whitespace-nowrap text-white rounded-lg dark:text-white">
+                CLIENTS
+              </span>
+            </span>
+          </li>
+          {level > 9 ? (
             <li>
-               <span className="flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group">
-                  <span className="ml-3">Dashboard</span>
-               </span>
+              <span
+                className="transform transition-transform ease-in hover:scale-105 cursor-pointer flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group"
+                onClick={() => {
+                  setSwitching("Users"),
+                    setHiding(false),
+                    setTimeout(() => setHiding(true), 1000);
+                }}
+              >
+                <img src={usersPic} className="w-6 md:w-[30px] lg:-[40px]" />
+                <span className="md:text-sm lg:text-md flex-1 ml-3 whitespace-nowrap text-white rounded-lg dark:text-white">
+                  USERS
+                </span>
+              </span>
             </li>
-            <li>
-               <Link to='/' className="transform transition-transform ease-in hover:scale-105 cursor-pointer flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group">
-                  <img src={homeBtn} className='w-6 md:w-[30px] lg:w-[40px] flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white' />
-                  <span className="md:text-sm lg:text-md ml-3 text-white rounded-lg dark:text-white">Home</span>
-               </Link>
-            </li>
-            <li>
-               <span
-                  className="transform transition-transform ease-in hover:scale-105 cursor-pointer flex items-center p-2 rounded-lg dark:text-white dark:hover:bg-gray-700 group"
-                  onClick={() => setSwitching('Client')}
-               >
-                  <img className="w-6 md:w-[30px] lg:-[40px]" src={client} />
-                  <span className="md:text-sm lg:text-md flex-1 ml-3 whitespace-nowrap text-white rounded-lg dark:text-white">
-                     CLIENTS
-                  </span>
-               </span>
-            </li>
-            {level > 9 ?             
-               <li>
-                  <span className="transform transition-transform ease-in hover:scale-105 cursor-pointer flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group" onClick={( ) => {setSwitching('Users'), setHiding(false),setTimeout(() => setHiding(true),1000)}}>
-                     <img src={usersPic} className='w-6 md:w-[30px] lg:-[40px]' />
-                     <span className="md:text-sm lg:text-md flex-1 ml-3 whitespace-nowrap text-white rounded-lg dark:text-white">
-                        USERS
-                     </span>
-                  </span>
-               </li> 
-               : null}
-            {/* <li className='transform transition-transform ease-in hover:scale-105 cursor-pointer'>
+          ) : null}
+          {/* <li className='transform transition-transform ease-in hover:scale-105 cursor-pointer'>
                <span onClick={( ) => setSwitching('Docs')} className="flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group">
                   <img src={survey} className='w-6 md:w-[30px] lg:-[40px]' /> 
                   <span className="md:text-sm lg:text-md flex-1 ml-3 whitespace-nowrap text-white rounded-lg dark:text-white">Docs</span>
                </span> 
             </li> */}
-            {level > 9 ?   
-               <li className='transform transition-transform ease-in hover:scale-105 cursor-pointer'>
-                  <span onClick={( ) => setSwitching('')} className="flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group">
-                     <img src={sqlProps} className='w-6 md:w-[30px]' /> 
-                     <Link to='/sqlProp' className="md:text-sm lg:text-md flex-1 ml-3 whitespace-nowrap text-white rounded-lg dark:text-white">Sql Properties</Link>
-                  </span> 
-               </li> 
-               : null}
-            <li>
-               <span onClick={handleGoPfP} className="transform transition-transform ease-in hover:scale-105 cursor-pointer flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group">
-                  <img src={userPfp} className='w-6 md:w-[30px] lg:-[40px]' />
-                  <span className="md:text-sm lg:text-md flex-1 ml-3 whitespace-nowrap">{user}</span>
-               </span>
+          {level > 9 ? (
+            <li className="transform transition-transform ease-in hover:scale-105 cursor-pointer">
+              <span
+                onClick={() => setSwitching("")}
+                className="flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group"
+              >
+                <img src={sqlProps} className="w-6 md:w-[30px]" />
+                <Link
+                  to="/sqlProp"
+                  className="md:text-sm lg:text-md flex-1 ml-3 whitespace-nowrap text-white rounded-lg dark:text-white"
+                >
+                  Sql Properties
+                </Link>
+              </span>
             </li>
-            <li>
-               <span className="transform transition-transform ease-in hover:scale-105 cursor-pointer flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group" onClick={handleLogout}>
-                  <img src={IN} className='w-6 md:w-[40px] flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white' />
-                  <span className="flex-1 ml-3 whitespace-nowrap">Sign Out</span>
-               </span>
-            </li>
-         </ul>
+          ) : null}
+          <li>
+            <span
+              onClick={handleGoPfP}
+              className="transform transition-transform ease-in hover:scale-105 cursor-pointer flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group"
+            >
+              <img src={userPfp} className="w-6 md:w-[30px] lg:-[40px]" />
+              <span className="md:text-sm lg:text-md flex-1 ml-3 whitespace-nowrap">
+                {user}
+              </span>
+            </span>
+          </li>
+          <li>
+            <span
+              className="transform transition-transform ease-in hover:scale-105 cursor-pointer flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group"
+              onClick={handleLogout}
+            >
+              <img
+                src={IN}
+                className="w-6 md:w-[40px] flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"
+              />
+              <span className="flex-1 ml-3 whitespace-nowrap">Sign Out</span>
+            </span>
+          </li>
+        </ul>
       </div>
-   </aside>
-  )
+    </aside>
+  );
 }
--- src/client/components/Home/Home.jsx
+++ src/client/components/Home/Home.jsx
@@ -1,168 +1,130 @@
 /* eslint-disable no-unused-vars */
-import Nav from '../Nav'
-import React,{useState,useEffect} from 'react'
-import {Link } from 'react-router-dom'
-import { auth, fs,db } from '/src/client/Firebase.jsx'
-import useLogo from '../images/useLogo.png'
-import rocket from '../images/rocket.jpg'
-import User from '../User'
-import Footer from './Footer'
-import axios from 'axios'
-import Loading from '../Loading'
-import Title from '../../Title'
-import fast from '../images/fast.jpg'
-import ai from '../images/Ai-home.jpg'
-import automation from '../images/automation-home.jpg'
-
-
-
+import Nav from "../Nav";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { auth, fs, db } from "/src/client/Firebase.jsx";
+import useLogo from "../images/useLogo.png";
+import rocket from "../images/rocket.jpg";
+import User from "../User";
+import Footer from "./Footer";
+import axios from "axios";
+import Loading from "../Loading";
+import Title from "../../Title";
+import fast from "../images/fast.jpg";
+import ai from "../images/Ai-home.jpg";
+import automation from "../images/automation-home.jpg";
 
 export default function Home() {
+  const [user, setUser] = useState("");
+  const [level, setLevel] = useState("");
+  const [uuid, setUuid] = useState("");
 
+  // useEffect(() => {
+  //   // Send user data to the server whenever it changes
+  //   axios.post('http://localhost:5173/', { user })
+  //     .then(response => {
+  //       console.log(response.data); // Log the response from the server if needed
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // }, [user]);
 
+  const [isVisible, setIsVisible] = useState(true);
+  const [zIndex, setZIndex] = useState(0);
+  const [fadeIn, setFadeIn] = useState(true);
 
+  useEffect(() => {
+    // Set the highest z-index value when the component is mounted
+    setZIndex(9999);
 
+    // After 500ms, set the visibility to false (display none)
+    const timeout = setTimeout(() => {
+      setIsVisible(false);
+    }, 800);
 
-    const [user,setUser] = useState('')
-    const [ level,setLevel] = useState('')
-    const [uuid,setUuid] = useState('')
+    // Clean up the timeout when the component is unmounted
+    return () => clearTimeout(timeout);
+  }, []);
 
-    
+  const [imageUrl, setImageUrl] = useState(
+    "https://source.unsplash.com/random/1600x900/?marketing"
+  );
 
-    // useEffect(() => {
-    //   // Send user data to the server whenever it changes
-    //   axios.post('http://localhost:5173/', { user })
-    //     .then(response => {
-    //       console.log(response.data); // Log the response from the server if needed
-    //     })
-    //     .catch(error => {
-    //       console.error(error);
-    //     });
-    // }, [user]);
-    
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFadeIn(true);
+      setImageUrl(
+        `https://source.unsplash.com/random/1600x900/?technology&t=${Date.now()}`
+      );
+    }, 12000);
 
-
-
-const [isVisible, setIsVisible] = useState(true);
-const [zIndex, setZIndex] = useState(0);
-
-useEffect(() => {
-  // Set the highest z-index value when the component is mounted
-  setZIndex(9999);
-
-  // After 500ms, set the visibility to false (display none)
-  const timeout = setTimeout(() => {
-    setIsVisible(false);
-  }, 800);
-
-  // Clean up the timeout when the component is unmounted
-  return () => clearTimeout(timeout);
-}, []);
-
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-      <>
-        <div className='bg-gray-100'>
-          <Nav />
+    <>
+      <div className={`${!isVisible ? "hidden" : "absolute w-full z-[100]"}`}>
+        <Loading />
+      </div>
+      <div className="relative h-screen">
+        <div className="absolute z-50 top-2 inset-0 opacity-100">
+          <Nav
+            user={user}
+            level={level}
+            uuid={uuid}
+            setUser={setUser}
+            setLevel={setLevel}
+            setUuid={setUuid}
+          />
         </div>
+        <img
+          className={`object-cover w-full h-full transition-opacity duration-1000 ${
+            fadeIn ? "opacity-75" : "opacity-0"
+          }`}
+          src={imageUrl}
+          alt="Header Background"
+        />
+        <div className="absolute inset-0 bg-black opacity-25" />
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+          <img className="w-[400px] h-[200px]" src={useLogo} alt="Logo" />
+          <h1 className="text-5xl text-white font-bold mb-4">
+            Welcome to Squarelion Media Pilot
+          </h1>
+          <p className="text-2xl text-white mb-4">
+            An innovative and rapid marketing solution. Kindly be aware that
+            Media Pilot is currently undergoing internal development.
+          </p>
+          <div className="flex justify-center gap-10">
+            <span className="flex flex-col ">
+              <img
+                className="w-[90px] h-[90px] mr-4 rounded-full"
+                src={rocket}
+                alt="Logo 1"
+              />
+              <p className="text-2xl mt-2 text-white mb-4">Fast</p>
+            </span>
 
-        <div className="bg-gradient-to-t from-slate-900 via-slate-800 to-slate-900">
-          <div className='flex flex-col items-center justify-center py-10'>
-            <h1 className='text-white text-center text-3xl font-bold mb-5'>
-              Welcome to Squarelion Agency
-             
-            </h1>
-         
-            <div className='flex flex-col md:flex-row items-center justify-center'>
-              <div className='md:w-1/2 flex flex-col ml-10 '>
-                <h1 className='text-left  text-white text-3xl font-bold mb-2  flex gap-2'> Marketing the faster way!  <img src={rocket}  className='w-[50px] rounded-full'/> </h1>
-                <p className='text-white text-left'>
-                  Media Pilot is a social media automation tool that helps you
-                  manage your social media accounts with ease. With Media Pilot,
-                  you can schedule posts, track analytics, and engage with your
-                  audience all in one place.
-                </p>
-              </div>
-              <div className='md:w-1/2'>
-                <img src={useLogo} alt='logo' className='w-64 mx-auto' />
-              </div>
-            </div>
+            <span className="flex flex-col ">
+              <img
+                className="w-[90px] h-[90px] mr-4 rounded-full"
+                src={fast}
+                alt="Logo 2"
+              />
+              <p className="text-2xl mt-2 text-white mb-4">Efficient</p>
+            </span>
+
+            <span className="flex flex-col ">
+              <img
+                className="w-[90px] h-[90px] rounded-full"
+                src={ai}
+                alt="Logo 3"
+              />
+              <p className="text-2xl mt-2 text-white mb-4">Automation</p>
+            </span>
           </div>
         </div>
-
-        <div className='bg-gray-100'>
-          <div className='container mx-auto py-10'>
-            <h2 className='text-3xl font-bold text-center mb-10'>
-              How we are changing social media marketing ?
-            </h2>
-            <div className='flex flex-col md:flex-row items-start justify-center'>
-              <div className='md:w-1/3'>
-                <img src={fast} alt='image1' className='mb-5   w-[200px] h-[200px] m-auto rounded-full' />
-                <p className='text-center'>
-                  With Media Pilot, you can create content to all your social
-                  media accounts at once, saving you time and effort.
-                </p>
-              </div>
-              <div className='md:w-1/3'>
-                <img src={ai} alt='image2' className='mb-5  m-auto  w-[200px] h-[200px] rounded-full' />
-                <p className='text-center'>
-                  Media Pilot uses artificial intelligence to help you create
-                  engaging content that will get more likes, comments, and
-                  shares.
-                </p>
-              </div>
-              <div className='md:w-1/3'>
-                <img src={automation} alt='image3' className='mb-5 m-auto  w-[200px] h-[200px] rounded-full' />
-                <p className='text-center'>
-                 Media Pilot Automation helps you save time by automating your
-                  social media posts and channels, so you can focus on what matters most.
-
-                </p>
-              </div>
-            </div>
-            <div className='text-center mt-10'>
-              <button className='bg-blue-950 text-white py-2 px-4 rounded'>
-                Learn More WIP
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div className='bg-gray-100  '>
-          <div className='container mx-auto py-10 flex flex-col'>
-            <h2 className='text-3xl font-bold text-center mb-10'>
-              Contact Us
-            </h2>
-            <div className='flex flex-col md:flex-col items-center gap-3 justify-center '>
-              <div className=' m-auto'>
-                <p className='text-center'>
-                  Have a question or need help? Contact us and we'll get back to
-                  you as soon as possible.
-                </p>
-              </div>
-              
-              <div className=' m-auto'>
-          <section className="text-gray-600 body-font relative">
-            <label  className="leading-7  text-black">
-              Email : kris@shaananportfolio.com 
-            </label>
-            </section>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-        <div className="bg-gradient-to-t from-slate-900 via-slate-800 to-slate-900">
-     <div className='container mx-auto py-10'>
-        <h2 className='text-3xl text-white font-bold text-center mb-10'>
-          Media pilot is still in internal testing phase many features are subject to change 
-
-        </h2>
-        </div>
-        </div>
-
-        <Footer />
-
-   
-  </>  )
+      </div>{" "}
+    </>
+  );
 }
