037ba9609a2037072bc0f55dc7af44831f308d2d
gilmon <gilmonpurry@hotmail.com>
2023-11-08 17:23:48 -0300

    updated designer page

--- src/client/components/Designer/DesignerHeader.jsx
+++ src/client/components/Designer/DesignerHeader.jsx
@@ -4,29 +4,40 @@ export default function DesignerHeader() {
 
     
 
-  return (
-    <div className='border-yellow-500 border-t-2 mt-3'>
-        <h1 className='mt-5 mb-5 ml-2'>
-Squarelion Designer Page 
+  return (<>
+     <div className='border-yellow-500 border-t-2 mt-3 bg-slate-700'>
+         <h1 className='text-4xl font-sans font-bold text-slate-200 text-center mt-2'>
+ Squarelion Designer Page 
+         </h1>
+       <h6 className='fixed bottom-1 left-2 text-[10px] text-white'>V 0.1</h6>
 
-        </h1>
-        <h6 className='fixed bottom-1 left-2 text-[10px]'>V 0.1</h6>
+     </div>
 
-<div className='flex flex-row items-center gap-4 ml-2'>
-<li>
-          Step 1 : Click choose file to upload image
-        </li>
+<div>
+  <article className="flex flex-row items-stretch justify-around w-full h-full p-10 bg-gray-100  dark:bg-slate-700">
 
-<li>
-  Step 2 : Click finish to complete 
-</li>
+<div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Click upload image</h5>
+    <p className="font-normal text-gray-700 dark:text-gray-400">Step 1</p>
 </div>
 
-<section className='mt-5 ml-2'>
-  <h1 className='mb-2'> Note to view the content click on view </h1>
-  <h1> Note for now you can only upload 1 image at a time</h1>
-</section>
+<div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Click finish to complete</h5>
+    <p className="font-normal text-gray-700 dark:text-gray-400">Step 2 :</p>
+</div>
 
-    </div>
-  )
+
+<div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">To see the instructions click on the view button</h5>
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">For now you can only upload 1 image at a time</h5>
+    <p className="font-normal text-gray-700 dark:text-gray-400">Note</p>
+    
+</div>
+
+</article>
+</div>
+
+
+
+  </>)
 }
