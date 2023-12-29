87401d7f4e18aef2b47c5a02cbf9db431210d3ad
Balans <gilmonpurry@hotmail.com>
2023-12-29 18:06:15 -0300

    Updates

--- assets/PartnerLogic-c01f4ea2.js
+++
@@ -1,1 +0,0 @@
-import{r as c,h as i}from"./index-f662206b.js";import"./Nav-9e453dca.js";function d({setPartner:n}){const a=async()=>{try{return i.collection("partner").onSnapshot(e=>{const o=e.docs.map(r=>({id:r.id,...r.data()}));o.sort((r,s)=>r.id-s.id),n(o)})}catch(t){console.error("Error fetching partner data:",t)}};c.useEffect(()=>{a()},[])}export{d as P};
