8cdcf9b2d6623e7c5c5ba893b15bcc37205babe9
gilmon <gilmonpurry@hotmail.com>
2024-01-10 10:27:15 -0300

    Updates

--- assets/PartnerLogic-838fe915.js
+++
@@ -1,1 +0,0 @@
-import{r as c,h as i}from"./index-2bca826c.js";import"./Nav-717b94e8.js";function d({setPartner:n}){const a=async()=>{try{return i.collection("partner").onSnapshot(e=>{const o=e.docs.map(r=>({id:r.id,...r.data()}));o.sort((r,s)=>r.id-s.id),n(o)})}catch(t){console.error("Error fetching partner data:",t)}};c.useEffect(()=>{a()},[])}export{d as P};
