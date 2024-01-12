bec2c16d24a766a9ffe674070760d2137e3a6bae
Balanss <gilmonpurry@hotmail.com>
2024-01-12 11:56:19 -0300

    Updates

--- assets/Nav-a070d632.js
+++ assets/Nav-a070d632.js
@@ -1,4 +1,4 @@
-import{s as ye,t as Nr,v as jr,w as Hr,x as zr,C as $r,z as Bn,A as Gr,S as qr,B as Wr,D as Kr,r as _,h as Xr,R as Ys,u as Yr,a as Pt,F as Re,j as N,U as Zr,L as Fn,b as Jr}from"./index-b76bbe34.js";/**
+import{s as ye,t as Nr,v as jr,w as Hr,x as zr,C as $r,z as Bn,A as Gr,S as qr,B as Wr,D as Kr,r as _,h as Xr,R as Ys,u as Yr,a as Pt,F as Re,j as N,U as Zr,L as Fn,b as Jr}from"./index-02cb2f0f.js";/**
  * @license
  * Copyright 2017 Google LLC
  *
---
+++ assets/PartnerLogic-e9b18f45.js
@@ -0,0 +1,1 @@
+import{r as c,h as i}from"./index-02cb2f0f.js";import"./Nav-a070d632.js";function d({setPartner:n}){const a=async()=>{try{return i.collection("partner").onSnapshot(e=>{const o=e.docs.map(r=>({id:r.id,...r.data()}));o.sort((r,s)=>r.id-s.id),n(o)})}catch(t){console.error("Error fetching partner data:",t)}};c.useEffect(()=>{a()},[])}export{d as P};
