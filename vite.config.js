efccc06debf9b62e54e34b06e9c3e13406a9203f
gilmon <gilmonpurry@hotmail.com>
2024-04-19 13:26:25 -0300

    Update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: 'https://Balanss.github.io/Squarelion',
+  base: 'https://Balanss.github.io/Squarelion',
   plugins: [react()],
 
 })
